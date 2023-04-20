82aa21e803ec454b1943ccae107740053c302404
PPWangyc <ywang330@u.rochester.edu>
2023-04-20 19:25:08 -0400

    add

--- ACT_example/convert.py
+++ ACT_example/convert.py
@@ -8,18 +8,12 @@ CONFIG_PATH = os.path.join(ACT_PATH,'bids', 'config.json')
 OUTPUT_BIDS_PATH = os.path.join('/scratch/tbaran2_lab/ACT_BIDS')
 session='01'
 dicom_dirs = get_folder_list(os.path.join(ACT_PATH, 'dicom'))
-# print(dicom_dirs)
+
 print(len(dicom_dirs))
 for dicom_dir in dicom_dirs:
-    # print(dicom_dir)
     screen_id = dicom_dir.split('_')[-1]
-    # print(screen_id)
     dicom_dir_path = os.path.join(ACT_PATH, 'dicom', dicom_dir)
-    print(dicom_dir_path)
-    # command = 'dcm2bids -d {} -p {} -s {} -c {} -o {}'.format(dicom_dir_path, screen_id, SBJ,CONFIG_PATH, OUTPUT_BIDS_PATH)
-    # print(command)
     command = 'sbatch convert_step1.sh {} {} {} {} {}'.format(dicom_dir_path, screen_id, session,CONFIG_PATH, OUTPUT_BIDS_PATH)
     print(command)
     os.system(command)
-    break
     
--- ACT_example/convert_step1.sh
+++ ACT_example/convert_step1.sh
@@ -8,6 +8,6 @@ session=${3}
 CONFIG_PATH=${4}
 OUTPUT_BIDS_PATH=${5}
 
-dcm2bids -d $dicom_dir_path -p ${screen_id} -s $SBJ -c $CONFIG_PATH -o $OUTPUT_BIDS_PATH
-# pydeface /bids/subject/session/anat/T1w
-${OUTPUT_BIDS_PATH}/sub-${screen_id}/ses-${SBJ}/anat/sub-${screen_id}_ses-${session}_T1w.nii.gz
+dcm2bids -d $dicom_dir_path -p ${screen_id} -s $session -c $CONFIG_PATH -o $OUTPUT_BIDS_PATH
+
+pydeface ${OUTPUT_BIDS_PATH}/sub-${screen_id}/ses-${session}/anat/sub-${screen_id}_ses-${session}_T1w.nii.gz

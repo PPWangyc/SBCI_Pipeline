81178180fe2749fbd610d8fa092b24148a082b45
PPWangyc <ywang330@u.rochester.edu>
2023-05-05 14:02:47 -0400

    add

--- ACT_example/dti_rpe_minimal_process.sh
+++ ACT_example/dti_rpe_minimal_process.sh
@@ -51,7 +51,7 @@ export RUNLEVEL=3
 # /fod_overlay 
 # /tissue_align 
 # /gm_wm 
-mkdir -p $QC_DIR/residual $QC_DIR/corrupt $QC_DIR/mask $QC_DIR/fod_voxels $QC_DIR/fod_overlay $QC_DIR/tissue_align $QC_DIR/gm_wm
+# mkdir -p $QC_DIR/residual $QC_DIR/corrupt $QC_DIR/mask $QC_DIR/fod_voxels $QC_DIR/fod_overlay $QC_DIR/tissue_align $QC_DIR/gm_wm
 
 ########################### STEP 1 ###################################
 #	            Convert data to .mif format and denoise	   	         #
@@ -110,115 +110,119 @@ dwibiascorrect ants dwi_den_preproc.mif dwi_den_preproc_unbiased.mif -bias bias.
 # Create whole brain mask from bias corrected data - mask should be tight
 dwi2mask dwi_den_preproc_unbiased.mif mask.mif
 
+# create new bvecs and bvals files for the preprocessed data
+mrinfo dwi_den_preproc_unbiased.mif -export_grad_fsl dwi/bvecs dwi/bvals
 
-########################### STEP 2 ###################################
-#             Basis function for each tissue type                    #
-######################################################################
+mrconvert dwi_den_preproc_unbiased.mif dwi/dwi.nii.gz
 
-# Create a basis function from the subject's DWI data to estimate response function for spherical deconvolution. The "dhollander" function is best used for multi-shell acquisitions; it will estimate different basis functions for each tissue type. For single-shell acquisition, use the "tournier" function instead
+# ########################### STEP 2 ###################################
+# #             Basis function for each tissue type                    #
+# ######################################################################
 
-# -------------- Type of shell-acquisition  -------------- #
-### SINGLE-SHELL (single b-value) 
-# dwi2response tournier dwi_den_preproc_unbiased.mif response_wm.txt response_gm.txt response_csf.txt -voxels voxels.mif
-dwi2response tournier dwi_den_preproc_unbiased.mif response_wm.txt -voxels voxels.mif
+# # Create a basis function from the subject's DWI data to estimate response function for spherical deconvolution. The "dhollander" function is best used for multi-shell acquisitions; it will estimate different basis functions for each tissue type. For single-shell acquisition, use the "tournier" function instead
 
-# move files into QC folder 
-# mv response_wm.txt response_gm.txt response_csf.txt $QC_DIR/response/$SBJ
+# # -------------- Type of shell-acquisition  -------------- #
+# ### SINGLE-SHELL (single b-value) 
+# # dwi2response tournier dwi_den_preproc_unbiased.mif response_wm.txt response_gm.txt response_csf.txt -voxels voxels.mif
+# dwi2response tournier dwi_den_preproc_unbiased.mif response_wm.txt -voxels voxels.mif
 
-# Estimate fibre orientation distributions (FOD) from diffusion data using spherical deconvolution, using the basis functions estimated above
-# dwi2fod csd dwi.mif response_wm.txt wmfod.mif
-dwi2fod csd dwi_den_preproc_unbiased.mif response_wm.txt wmfod.mif
-# -------------------------------------------------------- #
+# # move files into QC folder 
+# # mv response_wm.txt response_gm.txt response_csf.txt $QC_DIR/response/$SBJ
 
-### MULTI-SHELL (more than 1 b-value)
-# dwi2response dhollander dwi_den_preproc_unbiased.mif response_wm.txt response_gm.txt response_csf.txt-voxels voxels.mif
+# # Estimate fibre orientation distributions (FOD) from diffusion data using spherical deconvolution, using the basis functions estimated above
+# # dwi2fod csd dwi.mif response_wm.txt wmfod.mif
+# dwi2fod csd dwi_den_preproc_unbiased.mif response_wm.txt wmfod.mif
+# # -------------------------------------------------------- #
 
-# Estimate fibre orientation distributions from diffusion data using spherical deconvolution, using the basis functions estimated above
-# dwi2fod msmt_csd dwi_den_preproc_unbiased.mif -mask mask.mif response_wm.txt wmfod.mif response_gm.txt gmfod.mif response_csf.txt csffod.mif
-# -------------------------------------------------------- #
+# ### MULTI-SHELL (more than 1 b-value)
+# # dwi2response dhollander dwi_den_preproc_unbiased.mif response_wm.txt response_gm.txt response_csf.txt-voxels voxels.mif
 
-# Create an image of the FODs overlaid onto the estimated tissues (Blue=WM; Green=GM; Red=CSF)
-# mrconvert -coord 3 0 wmfod.mif - | mrcat csffod.mif gmfod.mif - vf.mif
+# # Estimate fibre orientation distributions from diffusion data using spherical deconvolution, using the basis functions estimated above
+# # dwi2fod msmt_csd dwi_den_preproc_unbiased.mif -mask mask.mif response_wm.txt wmfod.mif response_gm.txt gmfod.mif response_csf.txt csffod.mif
+# # -------------------------------------------------------- #
 
-# Normalize the FODs to enable comparison between subjects
-# mtnormalise wmfod.mif wmfod_norm.mif gmfod.mif gmfod_norm.mif csffod.mif csffod_norm.mif -mask mask.mif
-mtnormalise wmfod.mif wmfod_norm.mif -mask mask.mif
+# # Create an image of the FODs overlaid onto the estimated tissues (Blue=WM; Green=GM; Red=CSF)
+# # mrconvert -coord 3 0 wmfod.mif - | mrcat csffod.mif gmfod.mif - vf.mif
 
-########################### STEP 3 ###################################
-#            Create a GM/WM boundary for seed analysis               #
-######################################################################
+# # Normalize the FODs to enable comparison between subjects
+# # mtnormalise wmfod.mif wmfod_norm.mif gmfod.mif gmfod_norm.mif csffod.mif csffod_norm.mif -mask mask.mif
+# mtnormalise wmfod.mif wmfod_norm.mif -mask mask.mif
 
-# Convert the anatomical image to .mif format, and then extract all five tissue catagories (1=GM; 2=Subcortical GM; 3=WM; 4=CSF; 5=Pathological tissue)
-mrconvert $ANAT anat.mif
+# ########################### STEP 3 ###################################
+# #            Create a GM/WM boundary for seed analysis               #
+# ######################################################################
 
-# Segment the anatomical image into the tissue types
-5ttgen fsl anat.mif 5tt_nocoreg.mif
+# # Convert the anatomical image to .mif format, and then extract all five tissue catagories (1=GM; 2=Subcortical GM; 3=WM; 4=CSF; 5=Pathological tissue)
+# mrconvert $ANAT anat.mif
 
-# Average the b0 images
-dwiextract dwi_den_preproc_unbiased.mif - -bzero | mrmath - mean mean_b0_processed.mif -axis 3
+# # Segment the anatomical image into the tissue types
+# 5ttgen fsl anat.mif 5tt_nocoreg.mif
 
-# Convert b0 average and 5tt image to NIFTI for co-registration
-mrconvert mean_b0_processed.mif mean_b0_processed.nii.gz
-mrconvert 5tt_nocoreg.mif 5tt_nocoreg.nii.gz
+# # Average the b0 images
+# dwiextract dwi_den_preproc_unbiased.mif - -bzero | mrmath - mean mean_b0_processed.mif -axis 3
 
-# Extract the first volume (gray matter) of the 5tt dataset
-fslroi 5tt_nocoreg.nii.gz 5tt_vol0.nii.gz 0 1
+# # Convert b0 average and 5tt image to NIFTI for co-registration
+# mrconvert mean_b0_processed.mif mean_b0_processed.nii.gz
+# mrconvert 5tt_nocoreg.mif 5tt_nocoreg.nii.gz
 
-# Use fsl to create a transformation matrix for co-registration between the tissue map and the b0 images 
-flirt -in mean_b0_processed.nii.gz -ref 5tt_vol0.nii.gz -interp nearestneighbour -dof 6 -omat diff2struct_fsl.mat
+# # Extract the first volume (gray matter) of the 5tt dataset
+# fslroi 5tt_nocoreg.nii.gz 5tt_vol0.nii.gz 0 1
 
-# Convert back to format that mrtrix can read
-transformconvert diff2struct_fsl.mat mean_b0_processed.nii.gz 5tt_nocoreg.nii.gz flirt_import diff2struct_mrtrix.txt 
+# # Use fsl to create a transformation matrix for co-registration between the tissue map and the b0 images 
+# flirt -in mean_b0_processed.nii.gz -ref 5tt_vol0.nii.gz -interp nearestneighbour -dof 6 -omat diff2struct_fsl.mat
 
-# Co-register the anatomical image to the diffusion image
-mrtransform 5tt_nocoreg.mif -linear diff2struct_mrtrix.txt -inverse 5tt_coreg.mif
+# # Convert back to format that mrtrix can read
+# transformconvert diff2struct_fsl.mat mean_b0_processed.nii.gz 5tt_nocoreg.nii.gz flirt_import diff2struct_mrtrix.txt 
 
-# Create a seed region along the GM/WM boundary
-5tt2gmwmi 5tt_coreg.mif gmwmSeed_coreg.mif
+# # Co-register the anatomical image to the diffusion image
+# mrtransform 5tt_nocoreg.mif -linear diff2struct_mrtrix.txt -inverse 5tt_coreg.mif
 
-
-########################### STEP 4 ###################################
-#                                QC                                 #
-######################################################################
-
-# --------------------------- QC for Step 1 --------------------------
-# View original diffusion data overlaid on top of the eddy-corrected data and colored in red
-# mrview dwi_den_preproc.mif -overlay.load raw_dwi.mif
-
-### Corrupt slices 
-# dwi_post_eddy.eddy_outlier_map indicates whether a slice is an outlier (1) or not (0), because of too much motion, eddy currents, or something else.
-cd dwifslpreproc-tmp-*
-
-# Calculate the total number of slices by multiplying the number of slices for a single volume by the total number of volumes in the dataset.
-totalSlices=`mrinfo dwi.mif | grep Dimensions | awk '{print $6 * $8}'`
-
-# The total number of 1�s in the outlier map is then calculated, and the percentage of outlier slices is generated by dividing the number of outlier slices by the total number of slices. If this number is greater than 10 - i.e., if more than 10 percent of the slices are flagged as outliers - you should consider removing the subject from further analyses.
-totalOutliers=`awk '{ for(i=1;i<=NF;i++)sum+=$i } END { print sum }' dwi_post_eddy.eddy_outlier_map`
-
-echo "scale=5; ($totalOutliers / $totalSlices * 100)/1" | bc | tee ${SBJ}_percentageOutliers.txt 
-
-mv ${SBJ}_percentageOutliers.txt $QC_DIR/corrupt
-
-cd ..
-
-# --------------------------- QC for Step 2 --------------------------
-### Brain mask 
-# mrview mask.mif -capture.folder $QC_DIR/mask -capture.prefix $SBJ -capture.grab
-
-### Basis function
-# View the response functions for each tissue type (response_*.txt files in the QC folder). The WM function should flatten out at higher b-values, while the other tissues should remain spherical
-
-### FOD estimation 
-# View the voxels used for FOD estimation (Blue=WM; Green=GM; Red=CSF)"
-# mrview dwi_den_preproc_unbiased.mif -overlay.load voxels.mif -capture.folder $QC_DIR/fod_voxels -capture.prefix $SBJ -capture.grab
-
-# Views the FODs overlaid on the tissue types (Blue=WM; Green=GM; Red=CSF)
-# mrview vf.mif -odf.load_sh wmfod.mif -capture.folder $QC_DIR/fod_overlay -capture.prefix $SBJ -capture.grab
+# # Create a seed region along the GM/WM boundary
+# 5tt2gmwmi 5tt_coreg.mif gmwmSeed_coreg.mif
 
 
-# --------------------------- QC for Step 3 --------------------------
-# Check alignment of the 5 tissue types before and after alignment (new alignment in red, old alignment in blue)
-# mrview dwi_den_preproc_unbiased.mif -overlay.load 5tt_nocoreg.mif -overlay.colourmap 2 -overlay.load 5tt_coreg.mif -overlay.colourmap 1 -capture.folder $QC_DIR/tissue_align -capture.prefix $SBJ -capture.grab
+# ########################### STEP 4 ###################################
+# #                                QC                                 #
+# ######################################################################
 
-# Check the seed region (should match up along the GM/WM boundary)
-# mrview dwi_den_preproc_unbiased.mif -overlay.load gmwmSeed_coreg.mif -capture.folder $QC_DIR/gm_wm -capture.prefix $SBJ -capture.grab
+# # --------------------------- QC for Step 1 --------------------------
+# # View original diffusion data overlaid on top of the eddy-corrected data and colored in red
+# # mrview dwi_den_preproc.mif -overlay.load raw_dwi.mif
+
+# ### Corrupt slices 
+# # dwi_post_eddy.eddy_outlier_map indicates whether a slice is an outlier (1) or not (0), because of too much motion, eddy currents, or something else.
+# cd dwifslpreproc-tmp-*
+
+# # Calculate the total number of slices by multiplying the number of slices for a single volume by the total number of volumes in the dataset.
+# totalSlices=`mrinfo dwi.mif | grep Dimensions | awk '{print $6 * $8}'`
+
+# # The total number of 1�s in the outlier map is then calculated, and the percentage of outlier slices is generated by dividing the number of outlier slices by the total number of slices. If this number is greater than 10 - i.e., if more than 10 percent of the slices are flagged as outliers - you should consider removing the subject from further analyses.
+# totalOutliers=`awk '{ for(i=1;i<=NF;i++)sum+=$i } END { print sum }' dwi_post_eddy.eddy_outlier_map`
+
+# echo "scale=5; ($totalOutliers / $totalSlices * 100)/1" | bc | tee ${SBJ}_percentageOutliers.txt 
+
+# mv ${SBJ}_percentageOutliers.txt $QC_DIR/corrupt
+
+# cd ..
+
+# # --------------------------- QC for Step 2 --------------------------
+# ### Brain mask 
+# # mrview mask.mif -capture.folder $QC_DIR/mask -capture.prefix $SBJ -capture.grab
+
+# ### Basis function
+# # View the response functions for each tissue type (response_*.txt files in the QC folder). The WM function should flatten out at higher b-values, while the other tissues should remain spherical
+
+# ### FOD estimation 
+# # View the voxels used for FOD estimation (Blue=WM; Green=GM; Red=CSF)"
+# # mrview dwi_den_preproc_unbiased.mif -overlay.load voxels.mif -capture.folder $QC_DIR/fod_voxels -capture.prefix $SBJ -capture.grab
+
+# # Views the FODs overlaid on the tissue types (Blue=WM; Green=GM; Red=CSF)
+# # mrview vf.mif -odf.load_sh wmfod.mif -capture.folder $QC_DIR/fod_overlay -capture.prefix $SBJ -capture.grab
+
+
+# # --------------------------- QC for Step 3 --------------------------
+# # Check alignment of the 5 tissue types before and after alignment (new alignment in red, old alignment in blue)
+# # mrview dwi_den_preproc_unbiased.mif -overlay.load 5tt_nocoreg.mif -overlay.colourmap 2 -overlay.load 5tt_coreg.mif -overlay.colourmap 1 -capture.folder $QC_DIR/tissue_align -capture.prefix $SBJ -capture.grab
+
+# # Check the seed region (should match up along the GM/WM boundary)
+# # mrview dwi_den_preproc_unbiased.mif -overlay.load gmwmSeed_coreg.mif -capture.folder $QC_DIR/gm_wm -capture.prefix $SBJ -capture.grab

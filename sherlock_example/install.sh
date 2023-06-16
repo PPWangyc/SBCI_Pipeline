26a22c3a79385d5ff0237346d3f3f1203eed4101
Yanchen Wang <ppwang@sh03-ln05.stanford.edu>
2023-06-16 00:30:56 -0700

    fix

--- sherlock_example/install.sh
+++ sherlock_example/install.sh
@@ -1,7 +1,8 @@
-module load gcc /9.1.0
-module load biology ants /2.3.1
-module load fsl /5.0.10 module load system git qt
-module load java /11.0.11
-module load contribs poldrack pigz /2.4
-module load freesurfer /6.0.1 mrtrix /3.0.3 dcm2niix /1.0.20211006 module load math matlab/R2017b
-module load anaconda /2009.03
+module load gcc/9.1.0
+module load biology ants/2.3.1
+module load fsl/5.0.10 module load system git qt
+module load java/11.0.11
+module load contribs poldrack pigz/2.4
+module load freesurfer/6.0.1 mrtrix/3.0.3 dcm2niix/1.0.20211006 
+module load math matlab/R2017b
+module load anaconda/2009.03
